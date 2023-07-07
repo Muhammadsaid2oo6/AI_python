a5d3ad116484e21910de500b3c8c3bd551b83a81
Muhammadsaid2oo6 <muhammadsaid06hasanboyev@gmail.com>
2023-07-07 11:17:53 +0500

    sumLists

---
+++ Sum_of_2_lists.py
@@ -0,0 +1,13 @@
+
+import random
+
+list1 = [random.randint(1, 100) for x in range(100)]
+list2 = [random.randint(1, 100) for x in range(100)]
+
+sum_list = []
+i = 0
+
+while i < len(list1):
+    sum_list.append(list1[i]+list2[i])
+    i += 1
+print(sum_list)
